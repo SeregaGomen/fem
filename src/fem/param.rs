dab2e90b786eaf3926a3b6a0eba50376d4355efa
SeregaGomen <gserega71@gmail.com>
2022-06-21 12:05:21 +0300

    Last

--- src/fem/param.rs
+++ src/fem/param.rs
@@ -22,7 +22,7 @@ enum ParamValueType {
 struct ParamValue<'a> {
     value_type: ParamValueType,
     value_str: &'a str,
-    value_fun: Option<fn(f64, f64, f64) -> f64>,
+    value_fun: Option<Box<dyn Fn(f64, f64, f64) -> f64 + Sync>>,
     value_vec: Vec<[f64; 2]>,
 }
 
@@ -30,8 +30,8 @@ impl<'a> ParamValue<'a> {
     fn new_str(value: &'a str) -> Self {
         Self { value_type: ParamValueType::String, value_str: value, value_fun: None, value_vec: Vec::new() }
     }
-    fn new_fun(value: fn(f64, f64, f64) -> f64) -> Self {
-        Self { value_type: ParamValueType::Function, value_str: "", value_fun: Some(value), value_vec: Vec::new() }
+    fn new_fun<F>(value: F) -> Self where F: Fn(f64, f64, f64) -> f64 + Sync + 'static {
+        Self { value_type: ParamValueType::Function, value_str: "", value_fun: Some(Box::new(value)), value_vec: Vec::new() }
     }
     fn new_vec(value: Vec<[f64; 2]>) -> Self {
         Self { value_type: ParamValueType::Vector, value_str: "", value_fun: None, value_vec: value }
@@ -69,12 +69,12 @@ impl<'a> Parameter<'a> {
         let predicate = ParamValue::new_str(pred);
         Self { p_type, value, predicate, direct }
     }
-    fn new_scalar_fun(p_type: ParamType, val: fn(f64, f64, f64) -> f64, pred: fn(f64, f64, f64) -> f64, direct: Direct) -> Self {
+    fn new_scalar_fun<V, P>(p_type: ParamType, val: V, pred: P, direct: Direct) -> Self where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         let value = ParamValue::new_fun(val);
         let predicate = ParamValue::new_fun(pred);
         Self { p_type, value, predicate, direct }
     }
-    fn new_vector_fun(p_type: ParamType, val: Vec<[f64; 2]>, pred: fn(f64, f64, f64) -> f64, direct: Direct) -> Self {
+    fn new_vector_fun<F>(p_type: ParamType, val: Vec<[f64; 2]>, pred: F, direct: Direct) -> Self where F: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         let value = ParamValue::new_vec(val);
         let predicate = ParamValue::new_fun(pred);
         Self { p_type, value, predicate, direct }
@@ -94,7 +94,7 @@ impl<'a> Parameter<'a> {
                 parser.value()
             }
             ParamValueType::Function => {
-                if let Some(fun) = self.value.value_fun  {
+                if let Some(fun) = &self.value.value_fun  {
                     Ok(fun(x[0], if x.len() > 1 { x[1] } else { 0. }, if x.len() > 2 { x[2] } else { 0. })) 
                 } else {
                     Err(FemError::InternalError)
@@ -128,7 +128,7 @@ impl<'a> Parameter<'a> {
                 }
             }
             ParamValueType::Function => {
-                let res = if let Some(fun) = self.predicate.value_fun {
+                let res = if let Some(fun) = &self.predicate.value_fun {
                     fun(x[0], if x.len() > 1 { x[1] } else { 0. }, if x.len() > 2 { x[2] } else { 0. } )
                 } else {
                     return Err(FemError::InternalError)
@@ -165,25 +165,25 @@ impl<'a> FEMParameter<'a> {
     pub fn add_stress_strain_curve_str(&mut self, value: Vec<[f64; 2]>, predicate: &'a str) {
         self.param.push(Parameter::new_vector_str(ParamType::StressStrainCurve, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
-    pub fn add_stress_strain_curve_fun(&mut self, value: Vec<[f64; 2]>, predicate: fn(f64, f64, f64) -> f64) {
+    pub fn add_stress_strain_curve_fun<P>(&mut self, value: Vec<[f64; 2]>, predicate: P) where P: Fn(f64, f64, f64) -> f64 + Sync + 'static, {
         self.param.push(Parameter::new_vector_fun(ParamType::StressStrainCurve, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
     pub fn add_young_modulus_str(&mut self, value: &'a str, predicate: &'a str) {
         self.param.push(Parameter::new_scalar_str(ParamType::YoungModulus, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
-    pub fn add_young_modulus_fun(&mut self, value: fn(f64, f64, f64) -> f64, predicate: fn(f64, f64, f64) -> f64) {
+    pub fn add_young_modulus_fun<V, P>(&mut self, value: V, predicate: P) where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         self.param.push(Parameter::new_scalar_fun(ParamType::YoungModulus, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
     pub fn add_poisons_ratio_str(&mut self, value: &'a str, predicate: &'a str) {
         self.param.push(Parameter::new_scalar_str(ParamType::PoissonsRatio, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
-    pub fn add_poisons_ratio_fun(&mut self, value: fn(f64, f64, f64) -> f64, predicate: fn(f64, f64, f64) -> f64) {
+    pub fn add_poisons_ratio_fun<V, P>(&mut self, value: V, predicate: P) where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         self.param.push(Parameter::new_scalar_fun(ParamType::PoissonsRatio, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
     pub fn add_thickness_str(&mut self, value: &'a str, predicate: &'a str) {
         self.param.push(Parameter::new_scalar_str(ParamType::Thickness, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
-    pub fn add_thickness_fun(&mut self, value: fn(f64, f64, f64) -> f64, predicate: fn(f64, f64, f64) -> f64) {
+    pub fn add_thickness_fun<V, P>(&mut self, value: V, predicate: P) where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         self.param.push(Parameter::new_scalar_fun(ParamType::Thickness, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
     pub fn set_eps(&mut self, eps: f64) {
@@ -192,28 +192,31 @@ impl<'a> FEMParameter<'a> {
     pub fn add_boundary_condition_str(&mut self, value: &'a str, predicate: &'a str, direct: Direct) {
         self.param.push(Parameter::new_scalar_str(ParamType::BoundaryCondition, value, predicate, direct));
     }
-    pub fn add_boundary_condition_fun(&mut self, value: fn(f64, f64, f64) -> f64, predicate: fn(f64, f64, f64) -> f64, direct: Direct) {
+    pub fn add_boundary_condition_fun<V, P>(&mut self, value: V, predicate: P, direct: Direct) where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         self.param.push(Parameter::new_scalar_fun(ParamType::BoundaryCondition, value, predicate, direct));
     }
     pub fn add_concentrated_load_str(&mut self, value: &'a str, predicate: &'a str, direct: Direct) {
         self.param.push(Parameter::new_scalar_str(ParamType::ConcentratedLoad, value, predicate, direct));
     }
+    pub fn add_concentrated_load_fun<V, P>(&mut self, value: V, predicate: P, direct: Direct) where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
+        self.param.push(Parameter::new_scalar_fun(ParamType::ConcentratedLoad, value, predicate, direct));
+    }
     pub fn add_volume_load_str(&mut self, value: &'a str, predicate: &'a str, direct: Direct) {
         self.param.push(Parameter::new_scalar_str(ParamType::VolumeLoad, value, predicate, direct));
     }
-    pub fn add_volume_load_fun(&mut self, value: fn(f64, f64, f64) -> f64, predicate: fn(f64, f64, f64) -> f64, direct: Direct) {
+    pub fn add_volume_load_fun<V, P>(&mut self, value: V, predicate: P, direct: Direct) where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         self.param.push(Parameter::new_scalar_fun(ParamType::VolumeLoad, value, predicate, direct));
     }
     pub fn add_surface_load_str(&mut self, value: &'a str, predicate: &'a str, direct: Direct) {
         self.param.push(Parameter::new_scalar_str(ParamType::SurfaceLoad, value, predicate, direct));
     }
-    pub fn add_surface_load_fun(&mut self, value: fn(f64, f64, f64) -> f64, predicate: fn(f64, f64, f64) -> f64, direct: Direct) {
+    pub fn add_surface_load_fun<V, P>(&mut self, value: V, predicate: P, direct: Direct) where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         self.param.push(Parameter::new_scalar_fun(ParamType::SurfaceLoad, value, predicate, direct));
     }
     pub fn add_pressure_load_str(&mut self, value: &'a str, predicate: &'a str) {
         self.param.push(Parameter::new_scalar_str(ParamType::PressureLoad, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
-    pub fn add_pressure_load_fun(&mut self, value: fn(f64, f64, f64) -> f64, predicate: fn(f64, f64, f64) -> f64) {
+    pub fn add_pressure_load_fun<V, P>(&mut self, value: V, predicate: P) where V: Fn(f64, f64, f64) -> f64 + Sync + 'static, P: Fn(f64, f64, f64) -> f64 + Sync + 'static {
         self.param.push(Parameter::new_scalar_fun(ParamType::PressureLoad, value, predicate, Direct::X | Direct::Y | Direct::Z));
     }
     pub fn add_variable(&mut self, name: &'a str, value: f64) {
